dec321498b86fb2771ae9d6cacade8e04adfd740
Matheus Medrado <matheusmedrado@ufu.br>
2026-04-20 15:46:31 -0300

    fiiz_buzz

--- algorithmswithgo.com/module01/fizz_buzz.go
+++ algorithmswithgo.com/module01/fizz_buzz.go
@@ -10,5 +10,20 @@ package module01
 // `fmt` package and print to standard out.
 // I wouldn't normally recommend this, but did
 // it here to make life easier for beginners.
+
+import "fmt"
+
 func FizzBuzz(n int) {
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("Fizz Buzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
 }
