b5a7587fbada44a3f0acde87d4f0891948f2284d
Matheus Medrado <matheusmedrado@ufu.br>
2026-04-20 14:01:30 -0300

    reverse

--- algorithmswithgo.com/module01/reverse.go
+++ algorithmswithgo.com/module01/reverse.go
@@ -1,4 +1,5 @@
 package module01
+import "strings"
 
 // Reverse will return the provided word in reverse
 // order. Eg:
@@ -6,6 +7,25 @@ package module01
 //   Reverse("cat") => "tac"
 //   Reverse("alphabet") => "tebahpla"
 //
+
+//This is a more efficient way of concatenating strings
 func Reverse(word string) string {
-	return ""
+
+	var rev strings.Builder
+	for i := len(word)-1; i>=0; i--{
+		rev.WriteByte(word[i])
+	}
+	return rev.String() 
 }
+
+//The "regular" way would be like this:
+// func Reverse(word string) string {
+
+// 	var rev string
+// 	for i := len(word)-1; i>=0; i--{
+// 		rev += string(word[i])
+// 	}
+// 	return rev 
+// }
+
+
