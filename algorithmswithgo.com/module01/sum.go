5023e0aeccf232dddd7ed81f128940f88e0b4c7b
Matheus Medrado <matheusmedrado@ufu.br>
2026-04-20 13:47:03 -0300

    sum

--- algorithmswithgo.com/module01/sum.go
+++ algorithmswithgo.com/module01/sum.go
@@ -3,5 +3,20 @@ package module01
 // Sum will sum up all of the numbers passed
 // in and return the result
 func Sum(numbers []int) int {
-	return 0
+	var sum int // we could also use sum := 0
+	for _, value := range numbers {
+		sum += value
+	}
+	return sum
 }
+
+// // There's also a recurssion method that we could use
+
+// func Sum(numbers []int) int {
+// 	if len(numbers) == 0{
+// 		return 0
+// 	}
+	
+// 	return numbers[0] + Sum(numbers[1:])
+
+// }
